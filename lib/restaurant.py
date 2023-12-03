c0c2e8c8c883a8a65e9ac7b549646b7c68adc2da
Everlyn <gachuieve32@gmail.com>
2023-12-03 19:49:39 +0300

    updated files

--- lib/restaurant.py
+++
@@ -1,9 +0,0 @@
-class Restaurant:
-    def __init__(self, name):
-        self._name = name
-
-    def get_name(self):
-        return self._name
-    
-restaurant = Restaurant("Sample Restaurant")
-
